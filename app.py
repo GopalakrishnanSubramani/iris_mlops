5244e4f43fdc05478244594f2bf23b3f7c3036bf
Gopalakrishnan Subramani <gopalinlatvia@gmail.com>
2023-02-17 16:29:27 +0200

    app run update

--- app.py
+++ app.py
@@ -64,7 +64,6 @@ def predict():
     return render_template("home.html", prediction_model= "Called Model  {} ".format(model),prediction_flower="\n The Flower is {} ".format(output[0]))
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True,host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
 
 
